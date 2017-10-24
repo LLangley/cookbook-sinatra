99fa168b1ef6773dffffd016bd49343f2688ba0c
Luke Langley <langley0171@gmail.com>
2017-10-24 18:09:01 +0100

    Boilerplate sinatra app

---
+++ app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  'The app.rb file will act as the Router and the Controller of my MVC app.'
+end
